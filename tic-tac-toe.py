0ac2c96ea1c16f4356c7533813e129d5a40ae65e
Dimitris Vidos <dvidos@gmail.com>
2020-12-30 13:33:15 -0500

    ttt computer can play at random

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,12 @@
 # program to investigate tic-tac-toe solutions
 import time
+import random
 
-
+def get(prompt, chars, default=None):
+    while True:
+        k = input(prompt)
+        if k in chars:
+            return k
 
 class TTT:
     def __init__(self):
@@ -83,48 +88,76 @@ class TTT:
             self.test_recursively()
             self.play(c, ' ')
     
-def get(prompt, chars, default=None):
-    while True:
-        k = input(prompt)
-        if k in chars:
-            return k
-
 class Player:
-    def __init__(self, name, symbol, is_user=True):
+    AI_RANDOM = 1
+
+    def __init__(self, name, symbol, intelligence=None):
         self.name = name
         self.symbol = symbol
-        self.is_user = is_user
+        self.intelligence = intelligence
+
+    def move(self, ttt: TTT):
+        if self.intelligence == None:
+            self._move_human(ttt)
+        elif self.intelligence == self.AI_RANDOM:
+            self._move_computer_random(ttt)
+
+    def _move_human(self, ttt: TTT):
+        while True:
+            k = get(f"{self.name} enter location to play (1-9): ", '123456789')
+            row = int((int(k)-1) / 3)
+            col = int((int(k)-1) % 3)
+            if not ttt.can_play(row, col):
+                print("Cannot play there")
+                continue
+            ttt.play([row, col], self.symbol)
+            break
+
+    def _move_computer_random(self, ttt: TTT):
+        while True:
+            r = random.randrange(9)
+            row = int(r / 3)
+            col = int(r % 3)
+            if not ttt.can_play(row, col):
+                continue
+            print(f"Computer plays at {r+1}")
+            ttt.play([row, col], self.symbol)
+            break
 
 
-ttt = TTT()
-ttt.print()
-player = 0
-players = ['Νικόλας', 'Μπαμπάς']
-while True:
-    k = get(f"Player {players[player]} enter location to play (1-9): ", '123456789')
-    row = int((int(k)-1) / 3)
-    col = int((int(k)-1) % 3)
-    c = 'X' if player == 0 else 'O'
-    if ttt.can_play(row, col):
-        ttt.play([row, col], c)
-        player = 1 if player == 0 else 0
-    else:
-        print("Cannot play there")
 
+def play_game(players):
+    ttt = TTT()
     ttt.print()
+    player = 0
+    while True:
+        players[player].move(ttt)
 
-    if ttt.has_won('X'):
-        print(f"{players[0]} won!")
-        break
+        ttt.print()
+        for p in range(2):
+            if ttt.has_won(players[p].symbol):
+                print(f"{players[p].name} won!")
+                return
+        if ttt.is_finished():
+            print("It's a tie!")
+            return
 
-    if ttt.has_won('O'):
-        print(f"{players[1]} won!")
-        break
+        player = (player + 1) % 2
+    
 
-    if ttt.is_finished():
-        print("It's a tie!")
-        break
-
-
+print("1 - human vs human")
+print("2 - human vs computer (human starts)")
+print("3 - computer vs human (computer starts)")
+print("4 - computer vs computer")
+t = get("Choose game type: ", '1234')
+ai = Player.AI_RANDOM  # we'll make this configurable later
+configurations = [
+    [Player('Human 1', 'X'), Player('Human 2', 'O')],
+    [Player('Human', 'X'), Player('Computer', 'O', ai)],
+    [Player('Computer', 'X', ai), Player('Human', 'O') ],
+    [Player('Computer 1', 'X', ai), Player('Computer 2', 'O', ai)],
+]
+players = configurations[int(t) - 1]
+play_game(players)
 
 
